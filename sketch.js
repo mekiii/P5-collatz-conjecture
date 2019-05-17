1e4037a0212992a5922e8db2cfbfa511ac0a8a4e
Meki <you@example.com>
2019-05-17 17:51:50 +0200

    restructuring collatz functions and playing around with plotting odd numbers

--- sketch.js
+++ sketch.js
@@ -1,17 +1,22 @@
-let step;
-let sequence = [
-  [1]
-];
-let isLerping = false;
-let level, y, lerpingy, lerpingx1, lerpingx2;
-let row = 0;
+let yDist = 25;
+let xDist = 40;
+let startingPoint;
+let isFirstCollatz;
+let withText;
+let pos = [];
+let counter;
+let variance;
 
 function setup() {
-  step = 1;
   createCanvas(windowWidth, windowHeight);
   frameRate(5);
   background(255);
-  lerpingy = 0;
+  colorMode(RGB, 255, 255, 255, 1);
+  startingPoint = createVector(10, 10);
+  isFirstCollatz = true;
+  withText = false;
+  counter = 0;
+  variance = 0;
 }
 
 function collatzify(maxLevel) {
@@ -19,6 +24,7 @@ function collatzify(maxLevel) {
     [1]
   ];
   for (let i = 0; i < maxLevel; i++) {
+    //Create empty level array for following sequence numbers
     let level = [];
     sequence[i].forEach((num) => {
       //If odd number follows
@@ -32,28 +38,115 @@ function collatzify(maxLevel) {
   }
   return sequence;
 }
+console.log(collatzify(10))
+
+
+function calcCollatzFollower(num) {
+  let follower = [];
+  //Even number follows
+  follower.push(2 * num);
+  //Check if there's an odd number following
+  if (num % 3 == 1 && num % 2 == 0) {
+    follower.push((num - 1) / 3);
+  }
+  return follower;
+}
+
+
+//draw follower of number num from the position it's been drawn;
+function drawFollower(num, numPos) {
+  noStroke();
+  noFill();
+  let follower = calcCollatzFollower(num);
+  //first number is always even
+  let evenFollower = follower[0];
+  let evenPos = createVector(numPos.x + xDist, numPos.y + yDist);
+  if (withText) {
+    fill(0, 0, 0, 1);
+    textSize(12);
+    text(evenFollower, evenPos.x, evenPos.y);
+  }
+  stroke(0, 0, 0, 0.3);
+  strokeWeight(3);
+  line(numPos.x, numPos.y, evenPos.x, evenPos.y);
+  if (follower[1]) {
+    //second number is odd
+    noStroke();
+    noFill();
+    let oddFollower = follower[1];
+    let oddPos = createVector(numPos.x - xDist, noise(random(numPos.y - variance, numPos.y + variance)) * 2 * (numPos.y + yDist));
+    if (withText) {
+      fill(0, 0, 0, 1);
+      textSize(12);
+      text(oddFollower, oddPos.x, oddPos.y);
+    }
+    stroke(0, 0, 0, 0.3);
+    strokeWeight(2);
+    line(numPos.x, numPos.y, oddPos.x, oddPos.y);
+    return [evenFollower, evenPos, oddFollower, oddPos];
+  }
+  return [evenFollower, evenPos];
+}
+
+
+
+
+function draw() {
+  if (isFirstCollatz) {
+    pos.push(drawFollower(1, startingPoint));
+    isFirstCollatz = false;
+  } else {
+    if (counter < 30) {
+
+      if (counter === 3) {
+        pos[0].pop();
+        pos[0].pop();
+        console.log(pos[0]);
+      }
+      let temp = [];
+      pos.forEach((num) => {
+        temp.push(drawFollower(num[0], num[1]));
+        if (num[2]) {
+          temp.push(drawFollower(num[2], num[3]));
+        }
+      })
+      pos = temp;
+      variance+=0.5;
+      yDist+=0.5;
+    } else {
+      background(255,0.9);
+      isFirstCollatz = true;
+      counter = -1;
+      pos = [];
+      variance = 0;
+      yDist = 25;
+    }
+    
+  }
+  counter++;
+}
+
 
 /*
-function collatz(maxLevels){
-  let levels = [[1]]
-  while (levels.length < maxLevels) {
-      let level = [];
-      levels[levels.length-1].forEach(function(num){
-          const mod6 = ((num-4)%6);
-          const odd = (num-1)/3;
-          if (!mod6 && odd != 1) {
-              level.push(odd)
-          }
-          level.push(num*2);
-      })
-      levels.push(level);
+    if (pos.length ){
+      let evenFollower = pos[0][0];
+      let evenPos = pos [0][1];
+      pos.push(drawFollower(evenFollower, evenPos));
+      let oddFollower = pos[1][2];
+      let oddPos = pos [3];
+      pos = drawFollower(oddFollower, oddPos)
+    }
+    else {
+      let evenFollower = pos[0];
+      let evenPos = pos [1];
+      pos = drawFollower(evenFollower, evenPos);
+    }
   }
-  return levels;
+  
 }
 */
 
-console.log(collatzify(15));
-
+/*
 function draw() {
   if (row < 40) {
     fill(0);
@@ -90,6 +183,4 @@ function draw() {
     sequence.push(level);
 
   }
-
-
-}
+}*/
