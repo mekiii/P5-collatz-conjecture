a98e7574e9b449af64d665c6d72622f05818c53a
Meki <you@example.com>
2019-05-31 11:45:33 +0200

    Playing around with spiralling motion

--- sketch.js
+++ sketch.js
@@ -6,44 +6,58 @@ let withText;
 let counter;
 let tSize;
 let followers = [];
+let yDistOdd;
+let xDistOdd;
 
 class collatzFollower {
-  constructor(predecessor, prePos) {
+  constructor(predecessor, prePos, radius, angle) {
     this.sprePos = prePos;
     this.even = 2 * predecessor;
     this.position = {};
+    this.radius = radius;
+    this.angle = radians(angle);
+    this.x = 0;
+    this.y = 0;
+    this.center = createVector(windowWidth/2, windowHeight/2);
     this.position.even = this.calcPosition(false, prePos);
     this.hasOddFollower;
     //Check if there's an odd number following
     if (predecessor % 3 == 1 && predecessor % 2 == 0) {
+      yDistOdd += 3;
+      xDistOdd += 5;
       this.odd = (predecessor - 1) / 3;
       this.position.odd = this.calcPosition(true, prePos);
       this.hasOddFollower = true;
     } else {
       this.hasOddFollower = false;
     }
-    this.tempPosEven = createVector(this.sprePos.x, this.sprePos.y);
-    this.tempPosOdd = createVector(this.sprePos.x, this.sprePos.y);
+    this.tempPos = createVector(this.sprePos.x, this.sprePos.y);
     this.lerpCounter = 0;
-    this.lerpStep = 0.3;
+    this.lerpStep = 0.2;
   }
 
   calcPosition(isOdd, sprePos) {
     if (isOdd) {
-      return createVector(sprePos.x - xDist, sprePos.y + yDist);
+      this.x = 0.7*(sprePos.x - 50);
+      this.y = 0.7*(sprePos.y - 50);
+      return createVector(this.x, this.y);
     } else {
-      return createVector(sprePos.x + xDist, sprePos.y + yDist);
+      this.x = sprePos.x + this.radius*cos(this.angle);
+      this.y = sprePos.y + this.radius*sin(this.angle);
+      return createVector(this.x, this.y);
     }
   }
 
   draw() {
-    let scolor = color(19, 37, 106, 0.4)
+    let scolor = color(84, 143, 77, 0.3)
+    fill(243, 179, 83, 0.3);
+    ellipse(this.sprePos.x, this.sprePos.y, 5, 5);
     stroke(scolor);
     strokeWeight(1);
-    if (abs(this.tempPosEven.x - this.position.even.x) > 0.05) {
-      this.tempPosEven.x = lerp(this.sprePos.x, this.position.even.x, this.lerpCounter);
-      this.tempPosEven.y = lerp(this.sprePos.y, this.position.even.y, this.lerpCounter);
-      line(this.sprePos.x, this.sprePos.y, this.tempPosEven.x, this.tempPosEven.y);
+    if (abs(this.tempPos.x - this.position.even.x) > 0.03) {
+      this.tempPos.x = lerp(this.sprePos.x, this.position.even.x, this.lerpCounter);
+      this.tempPos.y = lerp(this.sprePos.y, this.position.even.y, this.lerpCounter);
+      line(this.sprePos.x, this.sprePos.y, this.tempPos.x, this.tempPos.y);
     }
     if (withText) {
       noStroke();
@@ -52,13 +66,13 @@ class collatzFollower {
       text(this.even, this.position.even.x, this.position.even.y);
     }
     if (this.hasOddFollower) {
-      let scolor = color(243, 179, 83, 0.4);
+      let scolor = color(243, 179, 83, 0.3);
       stroke(scolor);
       strokeWeight(1);
-      if (abs(this.tempPosOdd.x - this.position.odd.x) > 0.05) {
-        this.tempPosOdd.x = lerp(this.sprePos.x, this.position.odd.x, this.lerpCounter);
-        this.tempPosOdd.y = lerp(this.sprePos.y, this.position.odd.y, this.lerpCounter);
-        line(this.sprePos.x, this.sprePos.y, this.tempPosOdd.x, this.tempPosOdd.y);
+      if (abs(this.tempPos.x - this.position.odd.x) > 0.03) {
+        this.tempPos.x = lerp(this.sprePos.x, this.position.odd.x, this.lerpCounter);
+        this.tempPos.y = lerp(this.sprePos.y, this.position.odd.y, this.lerpCounter);
+        line(this.sprePos.x, this.sprePos.y, this.tempPos.x, this.tempPos.y);
       }
       if (withText) {
         noStroke();
@@ -78,137 +92,43 @@ function reset() {
   isFirstCollatz = true;
   counter = 0;
   pos = [];
-  yDist = 15;
-  xDist = 20;
+  yDist = 20;
+  xDist = 25;
+  yDistOdd = 20;
+  xDistOdd = 25;
   angleEven = 0;
   angleOdd = 0;
-  startingPoint = createVector(1, 1);
+  startingPoint = createVector(windowWidth - 200, windowHeight - 200);
   tSize = 8;
   withText = false;
 }
 
 function setup() {
-  createCanvas(windowWidth * 2, windowHeight);
+  createCanvas(windowWidth , windowHeight);
   frameRate(15);
   colorMode(RGB, 255, 255, 255, 1);
   reset();
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 1000; i++) {
     if (i == 0) {
-      followers.push(new collatzFollower(1, startingPoint));
+      followers.push(new collatzFollower(1, startingPoint, i,i));
     } else {
       let predecessor = followers[i - 1];
-      if (predecessor.odd == 1){
-        xDist += 15;
-        yDist += 10;
-      }
-      followers.push(new collatzFollower(predecessor.even, predecessor.position.even));
+      followers.push(new collatzFollower(predecessor.even, predecessor.position.even, 20, 10*i));
       if (predecessor.hasOddFollower) {
-        followers.push(new collatzFollower(predecessor.odd, predecessor.position.odd));
+        followers.push(new collatzFollower(predecessor.odd, predecessor.position.odd, 20, 10*i));
       }
     }
   }
-  console.log(followers.length);
 }
 let element = 0;
 
 function draw() {
-
-  //background(221, 226, 218, 0.05);
   noStroke();
   noFill();
-  if(element < followers.length){
+  if (element < followers.length) {
     followers[element].draw();
-    if(followers[element].lerpCounter >= 1){
-    element++;
+    if (followers[element].lerpCounter >= 1) {
+      element++;
+    }
   }
-  }
-
-  //Start with the start number at the starting point
-  // if (isFirstCollatz) {
-  //   pos.push(drawFollower(1, startingPoint));
-  //   isFirstCollatz = false;
-  // } else {
-  //   if (counter < 35) {
-  //     let temp = [];
-  //     pos.forEach((num) => {
-  //       temp.push(drawFollower(num[0], num[1]));
-  //       if (num[2]) {
-  //         temp.push(drawFollower(num[2], num[3]));
-  //       }
-  //     })
-  //     pos = temp;
-  //   } else {
-  //     reset();
-  //   }
-  // }
-  // counter++;
 }
-
-
-// //Calculate the follower of the sequence term (num)
-// function calcCollatzFollower(num) {
-//   let follower = [];
-//   //Even number follows
-//   follower.push(2 * num);
-//   //Check if there's an odd number following
-//   if (num % 3 == 1 && num % 2 == 0) {
-//     follower.push((num - 1) / 3);
-//   }
-//   return follower;
-// }
-
-
-// //draw follower of number num from the position it's been drawn;
-// function drawFollower(num, numPos) {
-
-//   let follower = calcCollatzFollower(num);
-//   //first number is always even
-//   let evenFollower = follower[0];
-//   //Define new position
-//   let evenPos = calcNewPos(numPos, false)
-//   let evenColor = color(19, 37, 106, 0.4);
-//   drawConnection(evenColor, evenFollower, numPos, evenPos);
-//   if (follower[1]) {
-//     //second number is odd
-//     let oddFollower = follower[1];
-//     //Define new position
-//     let oddPos = calcNewPos(numPos, true);
-//     let selectedColor = color(243, 179, 83, 1);
-//     drawConnection(selectedColor, oddFollower, numPos, oddPos);
-//     return [evenFollower, evenPos, oddFollower, oddPos];
-//   }
-//   return [evenFollower, evenPos];
-// }
-
-
-// function drawConnection(scolor, ntext, startPos, endPos) {
-//   let tempPos = startPos;
-//   if (tempPos.y != endPos.y) {
-//     stroke(scolor);
-//     strokeWeight(1);
-//     tempPos.x = lerp(startPos.x, endPos.x, 0.05);
-//     tempPos.y = lerp(startPos.y, endPos.y, 0.05);
-//     line(startPos.x, startPos.y, tempPos.x, tempPos.y);
-//     if (withText) {
-//       noStroke();
-//       fill(19, 37, 106, 0.7);
-//       textSize(tSize + 5);
-//       text(ntext, endPos.x, endPos.y);
-//     }
-//     isDrawing = false;
-//   } else {
-//     isDrawing = true;
-//   }
-
-// }
-
-// function calcNewPos(term, isOdd) {
-//   if (isOdd) {
-//     let oddPos = createVector(term.x - xDist, term.y + yDist);
-//     return oddPos;
-//   } else {
-//     let evenPos = createVector(term.x + xDist, term.y + yDist);
-//     return evenPos;
-//   }
-
-// }
